8aaffb64df7decfbaeedf251e7ffc665467054b5
noemievincent <noemie.vincent1@student.hepl.be>
2022-08-29 18:01:07 +0200

    converted px to em

--- app/resource/js/main.js
+++ app/resource/js/main.js
@@ -1,3 +1,19 @@
 document.body.classList.remove('no-js');
+document.body.classList.add('js-enabled');
 
-console.log("hello")
+handleContactForm();
+
+function handleContactForm() {
+    const contactBtns = document.querySelectorAll('.showContact');
+    const closeBtn = document.querySelector('.contact__close');
+
+    contactBtns.forEach((btn) => {
+        btn.addEventListener('click', (e) => {
+            document.body.classList.add('contact--show');
+        });
+    });
+
+    closeBtn.addEventListener('click', (e) => {
+        document.body.classList.remove('contact--show');
+    });
+}
